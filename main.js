76d27879ca7b326fc075aaeee04141228af3886f
wxyzcctv <2846629226@qq.com>
2021-06-14 09:19:39 +0800

    实现自动无缝无限轮播

--- main.js
+++ main.js
@@ -10,11 +10,31 @@ class Carousel extends Component {
     }
     rander() {
         this.root = document.createElement("div");
+        this.root.classList.add("Carousel");
         for (let recode of this.attribute.src) {
-            let child = document.createElement("img");
-            child.src = recode;
+            let child = document.createElement("div");
+            child.style.backgroundImage = `url(${recode})`;
             this.root.appendChild(child)
         }
+        let currentIndex = 0;
+        setInterval(() => {
+            let children = this.root.children;
+            let nextIndex = (currentIndex + 1) % children.length;//此时current会在1~children.length 之间不断循环
+
+            let current = children[currentIndex];
+            let next = children[nextIndex];
+
+            next.style.transition = "none";
+            next.style.transform = `translateX(${100 - nextIndex * 100}%)`;
+
+            setTimeout(() => {
+                next.style.transition = "";
+                current.style.transform = `translateX(${-100 - currentIndex * 100}%)`;
+                next.style.transform = `translateX(${-nextIndex * 100}%)`;
+                currentIndex = nextIndex;
+            }, 16)
+            // 16毫秒正好是浏览器刷新一帧的时间
+        }, 3000)
         return this.root;
     }
     mountTo(parent) {
