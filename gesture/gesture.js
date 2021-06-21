28f48e7b3bba9c97167e2e0d16bdfa510cb0198f
wxyzcctv <2846629226@qq.com>
2021-06-21 08:27:48 +0800

    获取到移动的速度，实现flick判断

--- gesture/gesture.js
+++ gesture/gesture.js
@@ -81,6 +81,11 @@ element.addEventListener("touchcancel", event => {
 let start = (point, context) => {
     // console.log("start", point.clientX, point.clientY);
     context.startX = point.clientX, context.startY = point.clientY;
+    context.points = [{
+        t: Date.now(),
+        x: point.clientX,
+        y: point.clientY,
+    }]
 
     context.isTap = true;
     context.isPan = false;
@@ -107,6 +112,12 @@ let move = (point, context) => {
         console.log(dx, dy);
         console.log('pan');
     }
+    context.points = context.points.filter(point => Date.now() - point.t < 500)
+    context.points.push({
+        t: Date.now(),
+        x: point.clientX,
+        y: point.clientY,
+    })
     // console.log("move", point.clientX, point.clientY);
 }
 let end = (point, context) => {
@@ -119,6 +130,21 @@ let end = (point, context) => {
     }
     if (context.isPress) {
         console.log("pressend");
+    }
+    context.points = context.points.filter(point => Date.now() - point.t < 500);
+
+    let d, v;
+    if (!context.points.length) {
+        v = 0;
+    } else {
+        d = Math.sqrt((point.clientX - context.points[0].x) ** 2 + (point.clientY - context.points[0].y) ** 2);
+        v = d / (Date.now() - context.points[0].t);
+    }
+    if (v > 1.5) {
+        console.log("flick");
+        context.isFlick = true;
+    } else {
+        context.isFlick = false;
     }
     // console.log("end", point.clientX, point.clientY);
 }
