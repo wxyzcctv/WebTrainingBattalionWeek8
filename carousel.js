f137d6a413025b185622689887386c5fa94ca6ac
wxyzcctv <2846629226@qq.com>
2021-06-25 08:59:29 +0800

    通过引入自定义的gesture和animation实现自动轮播和手动轮播

--- carousel.js
+++ carousel.js
@@ -1,4 +1,7 @@
-import { Component } from "./framewok.js"
+import { Component } from "./framewok.js";
+import { enableGesture } from "./gesture/gesture.js";
+import { Timeline, Animation } from './animation.js';
+import { ease } from './ease.js';
 export class Carousel extends Component {
     constructor() {
         super();
@@ -15,9 +18,56 @@ export class Carousel extends Component {
             child.style.backgroundImage = `url(${recode})`;
             this.root.appendChild(child)
         }
+        enableGesture(this.root);
+        let timeline = new Timeline();
+        timeline.start();
+
+        let children = this.root.children;
 
         let position = 0;
 
+        this.root.addEventListener('pan', event => {
+            let x = event.clientX - event.startX
+            let current = position - ((x - x % 520) / 520);
+
+            for (const offset of [-1, 0, 1]) {
+                let pos = current + offset;
+                pos = (pos % children.length + children.length) % children.length;
+                children[pos].style.transition = 'none';
+                children[pos].style.transform = `translateX(${- pos * 520 + offset * 520 + x % 520}px)`;
+            }
+        })
+
+        this.root.addEventListener('panend', event => {
+            let x = event.clientX - event.startX;
+            position = position - Math.round(x / 520);
+
+            for (const offset of [0, - Math.sign(Math.round(x / 520) - x + 260 * Math.sign(x))]) {
+                let pos = position + offset;
+                pos = (pos + children.length) % children.length;
+                children[pos].style.transition = '';
+                children[pos].style.transform = `translateX(${- pos * 520 + offset * 520}px)`;
+            }
+        })
+
+        setInterval(() => {
+            let children = this.root.children;
+            let nextIndex = (position + 1) % children.length;//此时current会在1~children.length 之间不断循环
+
+            let current = children[position];
+            let next = children[nextIndex];
+
+            next.style.transition = "none";
+            next.style.transform = `translateX(${520 - nextIndex * 520}px)`;
+
+            timeline.add(new Animation(current.style, "transform", - position * 520, - 520 - position * 520, 500, 0, ease, v => `translateX(${v}px)`))
+            timeline.add(new Animation(next.style, "transform", 520 - nextIndex * 520, - nextIndex * 520, 500, 0, ease, v => `translateX(${v}px)`))
+
+            position = nextIndex;
+            // 16毫秒正好是浏览器刷新一帧的时间
+        }, 3000)
+
+        /*
         this.root.addEventListener('mousedown', event => {
             let children = this.root.children;
             let startX = event.clientX;
@@ -52,6 +102,7 @@ export class Carousel extends Component {
             document.addEventListener('mouseup', up)
             // 在document上监听move和up主要是为了能解决鼠标移出指定区域之后无法监听move和up的bug
         })
+        */
 
         /*
         let currentIndex = 0;
--- main.js
+++ main.js
@@ -14,6 +14,6 @@ let a = <Carousel src={d}></Carousel>
 
 a.mountTo(document.body)
 
-let tl = new Timeline();
-tl.add(new Animation({ set a(v) { console.log(v) } }, "a", 0, 100, 1000, null))
-tl.start()
+// let tl = new Timeline();
+// tl.add(new Animation({ set a(v) { console.log(v) } }, "a", 0, 100, 1000, null))
+// tl.start()
